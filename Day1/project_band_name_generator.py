4861c1443adb59b77a373f6b9bf8913db14435b5
Amit_Raj <rajamitnine@gmail.com>
2023-11-04 23:02:22 +0530

    Project-1,Day-1,'Band Name Generator' and f-strings

---
+++ Day1/project_band_name_generator.py
@@ -0,0 +1,11 @@
+# Project - 1 : (Day - 1)
+
+print("Welcome to the 'Band Name Generator':")
+city = input("Which city did you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+# print("You band name could be " + city + " " + pet + ".")
+print(f"Your band name could be '{city} {pet}'.")
+
+# f-string :
+name = "Amit Raj"
+print(f"What is his name?\nHis name is {name}.")
