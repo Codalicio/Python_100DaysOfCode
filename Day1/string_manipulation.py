d90ea5157befe8be8b4b5f3aad732e085dec8583
Amit_Raj <rajamitnine@gmail.com>
2023-11-04 21:47:46 +0530

    String Manipulation, String concatenation and print() function

---
+++ Day1/string_manipulation.py
@@ -0,0 +1,22 @@
+# String Manipulation :
+
+print("Hello world!\nHello World!\nHello World!")
+
+# String Concatenation :
+print("Hello " + "Angela")
+print("Hello" + " Angela")
+print("Hello" + " " + "Angela")
+
+# Interactive Coding Exercise - 2 :
+# Debug the following code :
+
+# print(Day 1 - String Manipulation")
+# print("String Concatenation is done with the "+" sign.")
+#   print('e.g. print("Hello " + "world")')
+# print(("New lines can be created with a backslash and n.")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
