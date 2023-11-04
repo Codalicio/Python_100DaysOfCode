e4b2828439ae60ae276078cebb0df5ed84f0d06d
Amit_Raj <rajamitnine@gmail.com>
2023-11-04 22:13:48 +0530

    input() function, comments and len() function

---
+++ Day1/input.py
@@ -0,0 +1,27 @@
+# input() :
+
+# input("A prompt for the user")
+
+input("What is your name?\n")
+print("Hello " + input("What is your name?\n"))
+
+# Writing comments :
+# Comments are ignored and not executed!
+
+# Single line comments :
+# This is a single line comment!
+
+# Multi-line comments :
+"""This is
+a multi 
+line
+comment"""
+
+num1 = int(input("Enter the first number :\n"))
+num2 = int(input("Enter the second number :\n"))
+num3 = num1 * num2
+print(num3)
+
+# len() :
+print(len(input()))
+
