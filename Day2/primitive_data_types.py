e04d52022b2a9e8e55f93f5fb21ec0d0f88e2e38
Amit_Raj <rajamitnine@gmail.com>
2023-11-07 10:42:41 +0530

    Primitive Data Types : strings, integers, float, boolean and complex numbers

---
+++ Day2/primitive_data_types.py
@@ -0,0 +1,32 @@
+# Primitive Data Types :
+
+# String :
+
+print(len("Hello"))
+print("Hello"[0])
+print("Hello"[-1])
+
+print("123" + "456")
+print("Hello" + " World!")
+
+# Integer :
+
+print(123 + 345)
+
+# Large integers :
+
+a = 123_456_789
+print(a)
+
+# Float :
+
+pi = 3.14159
+print(pi)
+
+# Boolean : (True or False) :
+
+# Complex number :
+
+b = 6 + 7j
+print(b.real)
+print(b.imag)
