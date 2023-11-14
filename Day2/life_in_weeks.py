925cf70f2b8dd552015f6a395e9ed08f94c40277
Amit_Raj <rajamitnine@gmail.com>
2023-11-14 22:51:35 +0530

    life in weeks calculator using type conversion and f-string

---
+++ Day2/life_in_weeks.py
@@ -0,0 +1,10 @@
+age = int(input("Enter your age :\n"))
+
+years_left = (90 - age)
+weeks_left = (years_left * 52)
+# total_weeks = (90 * 52)
+# weeks_lived = (age * 52)
+
+# weeks_left = (total_weeks - weeks_lived)
+
+print(f"You have {weeks_left} weeks left.")
