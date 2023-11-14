c381ebc8ca9ceddc5ca5d6d9c82d0b5984af0f3b
Amit_Raj <rajamitnine@gmail.com>
2023-11-14 21:03:56 +0530

    type error, type checking and type conversion

---
+++ Day2/type_error_type_checking_type_conversion.py
@@ -0,0 +1,19 @@
+try :
+    print(len(12))
+except TypeError as t:
+    print(t)
+
+# num_char = len(input("What is your name?\n"))
+# print(f"Your name has {num_char} characters.")
+# print(type(num_char))
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+a = float(123)
+print(type(a))
+
+print(70 + float("100.5"))
+
+print(str(100) + str(240))
+
+
