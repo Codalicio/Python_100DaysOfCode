771f583b35b903bcbf375a21c94c6610c0dbba05
Amit_Raj <rajamitnine@gmail.com>
2023-11-15 00:11:26 +0530

    Changed the variable - 'total_bill' to 'bill'

--- Day2/project_tip_calculator.py
+++ Day2/project_tip_calculator.py
@@ -1,11 +1,11 @@
 print(f"Welcome to the tip calculator :")
 
-total_bill = float(input("What is the total bill? $"))
+bill = float(input("What is the total bill? $"))
 
 tip = int(input("What percentage tip would you like to give? 10, 12, or 15\n"))
-tip_calculated = ((tip / 100) * total_bill)
+tip_calculated = ((tip / 100) * bill)
 
-total_bill_after_tip = total_bill + tip_calculated
+total_bill_after_tip = bill + tip_calculated
 
 splitting_with = int(input("How many people to split the bill with?\n"))
 
