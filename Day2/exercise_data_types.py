07dd9947010e5cc4c8200750f02a3f6af591cafe
Amit_Raj <rajamitnine@gmail.com>
2023-11-14 21:19:06 +0530

    Exercise-Data Types

---
+++ Day2/exercise_data_types.py
@@ -0,0 +1,10 @@
+two_digit_number = input("Enter a two digit number :\n")
+
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+
+sum_of_two_digits = (int(first_number) + int(second_number))
+print(f"The sum of both the digits is {sum_of_two_digits}")
+
+sum = int(two_digit_number[0]) + int(two_digit_number[1])
+print(sum)
